8a0382583e59cb7a1707bd757e9f3590a16f3725
Chingiz Mammadov <info@chz.dev>
2023-08-20 00:36:35 +0400

    Removed dist folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Preset } from "unocss";
-export interface GridOptions {
-    gridTemplateAreas: Object;
-    gridTemplateColumns: Object;
-    gridTemplateRows: Object;
-}
-export declare function presetGridAreas(options: GridOptions): Preset;
-export default presetGridAreas;
